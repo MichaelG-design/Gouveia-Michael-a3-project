8c6a9b36412a6749f0edc383ffcb30b485df9178
Michael Gouveia <mtgouveia06@gmail.com>
2025-03-04 20:39:51 -0500

    First Push

---
+++ Gouveia-Michael-a3-project/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+
+namespace MohawkGame2D;
+
+
+public class Game
+{
+   
+
+
+    public void Setup()
+    {
+
+    }
+
+
+    public void Update()
+    {
+
+    }
+}
+
